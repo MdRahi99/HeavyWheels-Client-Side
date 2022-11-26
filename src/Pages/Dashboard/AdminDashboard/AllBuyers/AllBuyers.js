4fffddd2e4f5af719f70741acdf75130ef685249
Md Rahi <fhrfhr102030@gmail.com>
2022-11-26 12:50:16 +0600

    add all buyers and sellers list specifically in adout dashboard

--- src/Pages/Dashboard/AdminDashboard/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/AdminDashboard/AllBuyers/AllBuyers.js
@@ -7,7 +7,7 @@ const AllBuyers = () => {
   const { data: users = [] } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await fetch("http://localhost:5000/users");
+      const res = await fetch("http://localhost:5000/users/buyer");
       const data = await res.json();
       return data;
     },
